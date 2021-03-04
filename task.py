7fe88030ef023b70682d674321b0f117dcb3c646
Perchin <43394135+Perchin@users.noreply.github.com>
2021-03-03 21:41:10 -0600

    (PY-D0003) Missing module/function docstring Fixes #2

--- task.py
+++ task.py
@@ -2,7 +2,18 @@ import datetime
 import time
 taskList = []
 class Task:
+    """A class that represents a Task
+    """    
     def __init__(self, name, desc, createdDate, startDate, endDate):
+        """Constructor for Tasks
+
+        Args:
+            name (str): name of the task
+            desc (str): description of the task
+            createdDate (datetime.obj): date the task object was created
+            startDate (datetime.obj): date the task will start
+            endDate (datetime.obj): date the task will end
+        """        
         self.name = name
         self.desc = desc
         self.createdDate = createdDate
@@ -11,23 +22,29 @@ class Task:
     def __repr__(self):
         return f'Task name: {self.name} Description: {self.desc}Created Date: {self.created()} \nStart Date: {self.start()} End Date: {self.end()}'
     def getEnd(self):
+        '''Returns the end date of the self obj'''
         return self.endDate
     def getStart(self):
+        '''Returns the Start Date of the self obj'''
         return self.startDate
     def created(self):
+        '''Returns the Created Date of the self obj as a string'''
         return time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(self.createdDate))
     def start(self):
+        '''Returns the start date of the self obj as a string'''
         return self.startDate.strftime('%Y-%m-%d %H:%M %Z')
     def end(self):
+        '''Returns the end date of the self obj as a string'''
         return self.endDate.strftime('%Y-%m-%d %H:%M %Z')    
-    def pcreated(self):
-        print(time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(self.createdDate)))
-    def pstart(self):
-        print(time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(self.startDate)))
-    def pend(self):
-        print(time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(self.endDate)))
-
 
 def timeFormat(input):
+    """Formats the inputed datetime.obj to a string
+
+    Args:
+        input (datetime.obj): inputed datetime.obj
+
+    Returns:
+        str: returns the datetime.obj as a string
+    """    
     out =  time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(input))
     return out
